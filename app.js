bcdcca1c92fb98dc8d82c244af966ec2c095ec94
Armon Raphiel <araphiel@gmail.com>
2020-05-03 15:05:40 -0500

    major refactor

--- app.js
+++ app.js
@@ -2,35 +2,53 @@ const got = require('got')
 const cheerio = require('cheerio')
 
 
-async function getContent () {
-    
-        const links = [
-            'http://www.spcnet.tv/forums/showthread.php/17771-Smiling-Proud-Wanderer-Unabridged',
-            'http://www.spcnet.tv/forums/showthread.php/17771-Smiling-Proud-Wanderer-Unabridged/page2',
-        ]
+const links = [
+    'http://www.spcnet.tv/forums/showthread.php/17771-Smiling-Proud-Wanderer-Unabridged',
+    'http://www.spcnet.tv/forums/showthread.php/17771-Smiling-Proud-Wanderer-Unabridged/page2',
+]
 
-        links.forEach(async function (link){
-            try {
-                const { body } = await got(link)
-                const $ = cheerio.load(body)
-                const posts = $('#posts > li')
-                
-                posts.each(function (idx, elem) {
-                    const $elem = $(elem)
-                    const username = $elem.find('.username').text()
-                    
-                    if (username !== 'hhaung') return
-
-                    const regex = /chapter \d(\d?)/gi
-                    const chapterContents = $elem.find('.postrow').text()
-                    const contentTitle = chapterContents.match(regex)
-
-                    console.log(contentTitle)
-                })
-            } catch (error) {
-                console.log(error)
-            }
-        })
+async function getPages() {
+    return Promise.all(links.map(async link => {
+        const { body } = await got(link)
+        return body
+    }))
 }
 
-getContent()
+(async ()=> {
+    const arr = []
+    const translators = [
+        'hhaung',
+        'han solo',
+        'pokit'
+    ]
+
+    const pages = await getPages()
+    pages.forEach(page => {
+        const $ = cheerio.load(page)
+        const $posts = $('#posts > li')
+        $posts.each((idx, elem) => {
+            const $elem = $(elem)
+            const username = $elem.find('.username').text().toLowerCase().trim()
+            if (!translators.includes(username)) return
+            if ($elem.find('postcounter').text() === "#1") return
+
+            const regex = /chapter \d(\d?)/gi
+
+            const chapterContents = $(elem).find('.postrow').text()
+            
+            const title = $(elem).find('h2').text().replace(/(\r\n|\n|\r)/gm, "").trim()
+            const altTitle = 
+                chapterContents 
+                && chapterContents.match(regex)
+                && chapterContents.match(regex)[0]
+
+            arr.push({
+                title: title,
+                altTitle: altTitle,
+                content: chapterContents
+            })
+        })
+    })
+
+    console.log(arr)
+})()
