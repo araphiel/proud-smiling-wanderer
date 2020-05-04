9de98f8aec4bc5166e9f62cdd4c6b80e5daec5ae
Armon Raphiel <araphiel@gmail.com>
2020-05-03 20:00:15 -0500

    add quotes to fix markdown

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ for (let i = 1, k = 22; i < k; i++) {
                 : 'index'
 
             fs.outputFileSync(`${__dirname}/dist/markdown/${filename}.md`,
-                `---\ntitle: ${selectedTitle}\nauthor: Jin Yong (Louis Cha)\ntranslator: ${item.translator}\nchapter: ${chapter}\ndate: October 12, 1969\n---\n${item.content}`)
+                `---\ntitle: "${selectedTitle}"\nauthor: Jin Yong (Louis Cha)\ntranslator: ${item.translator}\nchapter: ${chapter}\ndate: October 12, 1969\n---\n${item.content}`)
         })
     } catch (error) {
         console.log(error)
